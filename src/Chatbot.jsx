c2296299c78d213a94c6eb973b4cd79cef5ffa1c
Saee Phalke <saeesphalke@gmail.com>
2025-08-10 11:29:08 -0700

    UI for selecting grade level and subject

--- src/Chatbot.jsx
+++ src/Chatbot.jsx
@@ -26,6 +26,13 @@ function Chatbox() {
     }
   ]);
 
+  const [grade, setGrade] = useState(null);
+  const [subject, setSubject] = useState(null);
+
+  const gradeLevels = ["Pre-K", "Kindergarten", "1st Grade", "2nd Grade", "3rd Grade", "4th Grade", "5th Grade", "6th Grade", "7th Grade", "8th Grade", "9th Grade", "10th Grade", "11th Grade", "12th Grade"
+    , "College", "Graduate School"
+  ];
+
   const handleSend = (innerMessage) => {
     setMessages([
       ...messages,
@@ -38,32 +45,66 @@ function Chatbox() {
   };
 
   return (
-    <div className="chatbox-wrapper">
-      <MainContainer className="chatbox-main">
-        <ChatContainer>
-          <MessageList
-            className="chatbot-message-list">
-            {messages.map((msg, idx) => (
-              <CSMessage
-                key={idx}
-                model={{
-                  message: msg.message,
-                  sentTime: "just now",
-                  sender: msg.sender,
-                  direction: msg.direction
-                }}
-              />
-            ))}
-          </MessageList>
-          <MessageInput
-            onSend={handleSend}
-            placeholder="Type message here"
-            attachButton={false}
-            sendDisabled={false}  // keep send via Enter
+    <>
+      <div>
+        <h2>Select a grade level and subject</h2>
+        <select
+          value={grade}
+          onChange={(e) => setGrade(e.target.value)}
+        >
+          <option value="" disabled selected option>
+            Select Grade Level
+          </option>
+          {gradeLevels.map((level, index) => (
+            <option key={index} value={index}>
+              {level}
+            </option>
+          ))}
+        </select>
+        <label>
+          <input
+            placeholder="Choose a Subject"
+            type="text"
+            value={subject}
+            onChange={(e) => setSubject(e.target.value)}
+          />
+        </label>
+        <button
+          onClick={() =>
+            handleSend(
+              `Selected Grade: ${gradeLevels[grade]}, Subject: ${subject}`
+            )
+          }
+        >
+          Send
+        </button>
+        <br /> <br />
+      </div>
+      <div className="chatbox-wrapper">
+        <MainContainer className="chatbox-main">
+          <ChatContainer>
+            <MessageList className="chatbot-message-list">
+              {messages.map((msg, idx) => (
+                <CSMessage
+                  key={idx}
+                  model={{
+                    message: msg.message,
+                    sentTime: "just now",
+                    sender: msg.sender,
+                    direction: msg.direction,
+                  }}
+                />
+              ))}
+            </MessageList>
+            <MessageInput
+              onSend={handleSend}
+              placeholder="Type message here"
+              attachButton={false}
+              sendDisabled={false} // keep send via Enter
             />
-        </ChatContainer>
-      </MainContainer>
-    </div>
+          </ChatContainer>
+        </MainContainer>
+      </div></>
   );
 }
 
